e43edf356791e9c95f0fafa99991a2eae791d036
Alan <31071359+amechu@users.noreply.github.com>
2018-09-29 11:50:49 -0300

    agregue clase stage

---
+++ PogoDeathMatch/Stage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Allegro\Allegro Wrapper\AllegroSprite.h"
+#include <vector>
+
+class Stage
+{
+public:
+	Stage(AllegroSprite* stageSprite_, unsigned radius_, unsigned centerX_, unsigned centerY_);
+	~Stage();
+
+	AllegroSprite* stageSprite;
+	unsigned radius;
+	unsigned centerX;
+	unsigned centerY;
+	BaseCharacter* player;
+	std::vector<BaseCharacter*> zombies;
+	void draw();
+	void addPlayer(BaseCharacter* player_);
+	void addZombie(BaseCharacter* zombie_);
+
+};
+
